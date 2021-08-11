717bc9b8e6230e193284b97fa30698ddc21d2ac2
sansamcl <63733772+sansamcl@users.noreply.github.com>
2021-08-11 14:42:19 -0500

    added Euler plot functionality

--- BedUpset/app.R
+++ BedUpset/app.R
@@ -11,78 +11,94 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Make an Upset plot from multiple bed files"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            fileInput(inputId="bedFiles",
-                      label="BedFiles",
-                      multiple=T),
-            uiOutput("sampleChoices"),
-            numericInput(inputId="fontScale",
-                         label="Font Scale",
-                         value=1),
-            numericInput(inputId="plotHeight",
-                         label="Plot Height",
-                         value=400)
-        ),
-        mainPanel(
-            plotOutput("upsetPlot")
-        )
+  
+  # Application title
+  titlePanel("Make Euler or Upset plot from multiple bed files"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      fileInput(inputId="bedFiles",
+                label="BedFiles",
+                multiple=T),
+      uiOutput("sampleChoices"),
+      radioButtons("plotType",
+                   "Plot Type",
+                   choices=c("Euler","Upset"),
+                   selected="Upset"),
+      numericInput(inputId="fontScale",
+                   label="Font Scale",
+                   value=1),
+      numericInput(inputId="plotHeight",
+                   label="Plot Height",
+                   value=400)
+    ),
+    mainPanel(
+      plotOutput("upsetPlot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  makeUpsetFromBeds <- function(BedFilenamesFull,BedFilenames){
+    Beds.df <- lapply(BedFilenamesFull,read.table)
+    Beds.gr <- lapply(Beds.df,
+                      GenomicRanges::makeGRangesFromDataFrame,
+                      seqnames.field="V1",
+                      start.field="V2",
+                      end.field="V3")
+    AllBeds.gr <- GenomicRanges::reduce(do.call("c",Beds.gr))
+    GetOverlapsWithAll <- function(bed.gr,all.gr){
+      GRovrlps <- all.gr[GenomicRanges::countOverlaps(all.gr,
+                                                      bed.gr)>0]
+      paste(GenomicRanges::seqnames(GRovrlps),
+            GenomicRanges::start(GRovrlps),
+            GenomicRanges::end(GRovrlps),
+            sep="_")
+    }
+    lst <- lapply(Beds.gr,GetOverlapsWithAll,AllBeds.gr)
+    nmes <- gsub(".bed","",basename(BedFilenames))
+    names(lst) <- nmes
+    upsetList <- UpSetR::fromList(lst)
     
-    makeUpsetFromBeds <- function(BedFilenamesFull,BedFilenames){
-        Beds.df <- lapply(BedFilenamesFull,read.table)
-        Beds.gr <- lapply(Beds.df,
-                          GenomicRanges::makeGRangesFromDataFrame,
-                          seqnames.field="V1",
-                          start.field="V2",
-                          end.field="V3")
-        AllBeds.gr <- GenomicRanges::reduce(do.call("c",Beds.gr))
-        GetOverlapsWithAll <- function(bed.gr,all.gr){
-            GRovrlps <- all.gr[GenomicRanges::countOverlaps(all.gr,
-                                                            bed.gr)>0]
-            paste(GenomicRanges::seqnames(GRovrlps),
-                  GenomicRanges::start(GRovrlps),
-                  GenomicRanges::end(GRovrlps),
-                  sep="_")
-        }
-        lst <- lapply(Beds.gr,GetOverlapsWithAll,AllBeds.gr)
-        nmes <- gsub(".bed","",basename(BedFilenames))
-        names(lst) <- nmes
-        upsetList <- UpSetR::fromList(lst)
-        plt <- UpSetR::upset(upsetList,
-                             nsets=length(lst),
-                             order.by = "freq",
-                             text.scale=input$fontScale)
-        plt
+    
+    eulerPlot <- plot(eulerr::euler(upsetList),quantities=T)
+    
+    
+    
+    plt <- UpSetR::upset(upsetList,
+                         nsets=length(lst),
+                         order.by = "freq",
+                         text.scale=input$fontScale)
+    
+    if(input$plotType=="Upset"){
+      plt
+    }else{
+      eulerPlot
     }
     
-    output$sampleChoices <- renderUI({
-        #checkboxGroupInput
-        shinyWidgets::multiInput("bedFileChoices", 
-                           "Choose Bed Files", 
-                           input$bedFiles$name,
-                           selected=input$bedFiles$name)
-    })
-    
-    datapaths <- reactive({
-        input$bedFiles$datapath[match(input$bedFileChoices,input$bedFiles$name)]
-        })
-    
-    output$upsetPlot <- renderPlot({
-        req(input$bedFiles)
-        makeUpsetFromBeds(datapaths(),
-                          input$bedFileChoices)
-    },height=reactive(input$plotHeight))
-
+  }
+  
+  output$sampleChoices <- renderUI({
+    #checkboxGroupInput
+    shinyWidgets::multiInput("bedFileChoices", 
+                             "Choose Bed Files", 
+                             input$bedFiles$name,
+                             selected=input$bedFiles$name)
+  })
+  
+  datapaths <- reactive({
+    input$bedFiles$datapath[match(input$bedFileChoices,input$bedFiles$name)]
+  })
+  
+  output$upsetPlot <- renderPlot({
+    req(input$bedFiles)
+    makeUpsetFromBeds(datapaths(),
+                      input$bedFileChoices)
+  },height=reactive(input$plotHeight))
+  
 }
 
 # Run the application 
